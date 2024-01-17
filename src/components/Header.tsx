209abc7aa1b3ffa347a066793f4257f27c6e01d7
dogyeong <82639552+yeomdogyeong@users.noreply.github.com>
2024-01-17 15:45:57 +0900

    [refactor]header_component화

---
+++ src/components/Header.tsx
@@ -0,0 +1,78 @@
+import { Link as ScrollLink } from "react-scroll";
+import { useState } from "react";
+import { motion } from "framer-motion";
+import useDarkModeStore from "../zustand/darkModeStore";
+
+const Header = () => {
+  const { isDarkMode, toggleDarkMode } = useDarkModeStore();
+  const [isOn, setIsOn] = useState(false);
+  const toggleSwitch = () => {
+    setIsOn(!isOn);
+    toggleDarkMode();
+  };
+
+  const spring = {
+    type: "spring",
+    stiffness: 700,
+    damping: 30,
+  };
+
+  return (
+    <div className="z-10 flex justify-between fixed top-0 w-full h-15 ">
+      <div
+        className={`w-1/3 text-3xl p-6 ${
+          isDarkMode ? "bg-[#635985]" : "bg-[#4682a9]"
+        }`}
+      >
+        <span className={`header ${isDarkMode ? "text-white" : ""}`}>
+          YDG's PF
+        </span>
+      </div>
+      <div
+        className={`header flex text-2xl justify-evenly w-full ${
+          isDarkMode ? "bg-[#635985]" : "bg-[#4682a9]"
+        } p-6`}
+      >
+        <div className="flex justify-between">
+          <ScrollLink
+            to="about"
+            smooth={true}
+            duration={500}
+            className=" hover:text-yellow-400"
+          >
+            about
+          </ScrollLink>
+        </div>
+        <div className="flex justify-between">
+          <ScrollLink
+            to="project"
+            smooth={true}
+            duration={700}
+            className=" hover:text-yellow-400"
+          >
+            project
+          </ScrollLink>
+        </div>
+        <div className="flex justify-between">
+          <ScrollLink
+            to="skills"
+            smooth={true}
+            duration={700}
+            className=" hover:text-yellow-400"
+          >
+            skills
+          </ScrollLink>
+        </div>
+        <div
+          className={isDarkMode ? "dark-mode switch" : "switch"}
+          data-ison={isOn}
+          onClick={toggleSwitch}
+        >
+          <motion.div className="handle" layout transition={spring} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
--- src/page/Main.tsx
+++ src/page/Main.tsx
@@ -1,10 +1,9 @@
 import { useState, useEffect } from "react";
 import "./Main.css";
-import { Link as ScrollLink } from "react-scroll";
-import { motion } from "framer-motion";
+
 import yeom from "../assets/yeomsia.jpg";
 import dowajoyak from "../assets/dowajoyak.png";
-
+import Header from "../components/Header";
 import signup from "../assets/signup.png";
 import Card from "../components/card";
 import Typewriter from "typewriter-effect";
@@ -34,12 +33,6 @@ function Main() {
     setIsFlipped(!isFlipped);
   };
 
-  const spring = {
-    type: "spring",
-    stiffness: 700,
-    damping: 30,
-  };
-
   useEffect(() => {
     const handleScroll = () => {
       const scrollPosition = window.scrollY;
@@ -57,60 +50,7 @@ function Main() {
 
   return (
     <>
-      <div className="z-10 flex justify-between fixed top-0 w-full h-15 ">
-        <div
-          className={`w-1/3 text-3xl p-6 ${
-            isDarkMode ? "bg-[#635985]" : "bg-[#4682a9]"
-          }`}
-        >
-          <span className={`header ${isDarkMode ? "text-white" : ""}`}>
-            YDG's PF
-          </span>
-        </div>
-        <div
-          className={`header flex text-2xl justify-evenly w-full ${
-            isDarkMode ? "bg-[#635985]" : "bg-[#4682a9]"
-          } p-6`}
-        >
-          <div className="flex justify-between">
-            <ScrollLink
-              to="about"
-              smooth={true}
-              duration={500}
-              className=" hover:text-yellow-400"
-            >
-              about
-            </ScrollLink>
-          </div>
-          <div className="flex justify-between">
-            <ScrollLink
-              to="project"
-              smooth={true}
-              duration={700}
-              className=" hover:text-yellow-400"
-            >
-              project
-            </ScrollLink>
-          </div>
-          <div className="flex justify-between">
-            <ScrollLink
-              to="skills"
-              smooth={true}
-              duration={700}
-              className=" hover:text-yellow-400"
-            >
-              skills
-            </ScrollLink>
-          </div>
-          <div
-            className={isDarkMode ? "dark-mode switch" : "switch"}
-            data-ison={isOn}
-            onClick={toggleSwitch}
-          >
-            <motion.div className="handle" layout transition={spring} />
-          </div>
-        </div>
-      </div>
+      <Header />
       <Star isDarkMode={isDarkMode} />
       <div
         id="about"
