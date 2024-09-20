b8b58f46f114225451e498c9dca4013d11cd5087
dogyeong <82639552+yeomdogyeong@users.noreply.github.com>
2024-09-21 00:58:41 +0900

    Fix : Footer 정렬 수정

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,22 +5,24 @@ const Footer = () => {
 
   return (
     <div
-      className={`z-10 flex sm:justify-center lg:justify-end items-center w-full h-24  ${
+      className={`z-10 flex-col items-center w-full h-24  ${
         isDarkMode ? "bg-[#443c68]" : "bg-[#91c8e4]"
       }  text-white`}
     >
-      <span className="text-white font-thin mr-6">Mady by yeomdogyeong</span>
-      <a
-        href="https://github.com/yeomdogyeong"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <img
-          className="rounded-full w-16 h-16 sm:mr-0 lg:mr-10"
-          src="https://github.com/yeomdogyeong.png"
-          alt="Profile icon"
-        />
-      </a>
+      <div className="flex items-center justify-center sm:justify-end sm:pr-4 h-full w-full">
+        <span className="text-white font-thin mr-6">Mady by yeomdogyeong</span>
+        <a
+          href="https://github.com/yeomdogyeong"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img
+            className="rounded-full w-16 h-16 sm:mr-0 lg:mr-10"
+            src="https://github.com/yeomdogyeong.png"
+            alt="Profile icon"
+          />
+        </a>
+      </div>
     </div>
   );
 };
