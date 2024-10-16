7f9f0300eda1ecea31f5869746938b8e561607ec
dogyeong <82639552+yeomdogyeong@users.noreply.github.com>
2024-10-16 17:12:38 +0900

    Fix: 필요없는 코드 삭제

--- src/page/Main.tsx
+++ src/page/Main.tsx
@@ -1,19 +1,12 @@
-import { useState } from "react";
 import "./Main.css";
 
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import Card from "../components/Card";
-import Typewriter from "typewriter-effect";
+
 import Star from "../components/Star";
 import About from "../components/About";
 import useDarkModeStore from "../zustand/useDarkModeStore";
-import Profile from "../components/Profile";
-import Graph from "../components/Graph";
-import { cardData, dogyeong } from "../dummyData/dummyData";
-import { Bounce } from "react-awesome-reveal";
-import { Slide } from "react-awesome-reveal";
-import { VerticalAni } from "../components/VerticalAni";
+
 import { Project } from "../components/Project";
 import { Connect } from "../components/Connect";
 function Main() {
@@ -31,52 +24,11 @@ function Main() {
         }`}
       >
         <div className="font-intelon flex flex-col justify-center items-center h-2/3 mb-10">
-          {/* <VerticalAni /> */}
-          {/* <Bounce
-            className={`mb-10 text-5xl ${
-              isDarkMode ? "text-[#f6d776]" : "text-[#91c8e4]"
-            }`}
-          >{`<ABOUT/>`}</Bounce> */}
-          <div className="flex items-center">
-            {/* <Typewriter
-              options={{
-                strings: [
-                  "React",
-                  "TypeScript",
-                  "JavaScript",
-                  "zustand",
-                  "Dart",
-                ],
-                autoStart: true,
-                loop: true,
-                wrapperClassName: "Typewriter__wrapper",
-              }}
-            /> */}
-            {/* <span>를 공부하고 있는</span> */}
-          </div>
+          <div className="flex items-center"></div>
         </div>
 
-        {/* <Slide className="flex justify-center items-center">
-          <Profile member={dogyeong} />
-        </Slide> */}
-
         <About />
-        {/* <div
-          id="project"
-          className={`border-dotted border w-full m-20 p-4 ${
-            isDarkMode ? "" : "border-gray-400"
-          }`}
-        ></div> */}
         <Project />
-        {/* <div id="skills">
-          <Bounce
-            className={`${isDarkMode ? "text-[#f6d776]" : ""} text-5xl mt-20`}
-          >
-            {`Connect`}
-          </Bounce>
-        </div> */}
-
-        {/* <Graph /> */}
         <Connect />
         <Footer />
       </div>
